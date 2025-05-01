cd831f383c96f69595594d1df279ea1950e3e82c
Lindsay RC Platt <lplatt@cuahsi.org>
2025-05-01 11:16:25 -0500

    rename 03_summarize PNG outputs

--- 03_summarize.R
+++ 03_summarize.R
@@ -16,7 +16,7 @@ p3 <- list(
   # Create a PNG showing boxplots of attribute values per CAMELS category
   tar_target(p3_camels_dist_by_huc_png,
              camels_boxplot_attribute_vals(
-               out_file = sprintf('03_summarize/figures/boxplot_%s.png', 
+               out_file = sprintf('03_summarize/figures/camels_boxplot_%s.png', 
                                   unique(p3_camels_data_numeric_long_grpCategory$category)),
                camels_data_long = p3_camels_data_numeric_long_grpCategory
              ),
@@ -27,7 +27,7 @@ p3 <- list(
   
   # Create a PNG showing boxplots of AmeriFlux variable values by vegetation category
   tar_target(p3_ameriflux_dist_by_veg_png,
-             ameriflux_boxplot_variable_vals('03_summarize/figures/boxplot_ameriflux.png',
+             ameriflux_boxplot_variable_vals('03_summarize/figures/ameriflux_boxplot.png',
                                              p2_ameriflux_data_long,
                                              p2_ameriflux_site_info),
              format = 'file'),
@@ -35,7 +35,7 @@ p3 <- list(
   # Create a PNG showing an example timeseries of AmeriFlux variable values for a single site
   tar_target(p3_ameriflux_timeseries_sites, c('US-A32')),
   tar_target(p3_ameriflux_timeseries_png,
-             ameriflux_timeseries(sprintf('03_summarize/figures/timeseries_ameriflux_%s.png',
+             ameriflux_timeseries(sprintf('03_summarize/figures/ameriflux_timeseries_%s.png',
                                           p3_ameriflux_timeseries_sites),
                                   p2_ameriflux_data_long,
                                   p3_ameriflux_timeseries_sites),
